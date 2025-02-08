e3d4402737db5ae0fa009067fd4fdaaa16fcdaee
Nagaadhithiya <141645478+adhithiya264@users.noreply.github.com>
2025-02-08 08:19:11 +0530

    Create Car.java

---
+++ Week 2 Task (java)/Car.java
@@ -0,0 +1,45 @@
+package oops;
+
+public class Car {
+	private String make;
+    private String model;
+    private int year;
+
+    // Constructor
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    // Getters and setters
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    // Method to start the engine
+    public void startEngine() {
+        System.out.println("The engine of " + make + " " + model + " is now running.");
+    }
+
+}
