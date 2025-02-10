907c0337851ceba4fa379e8c71a3756f530f43d4
Nagaadhithiya <141645478+adhithiya264@users.noreply.github.com>
2025-02-10 14:29:32 +0530

    Update task2.java

--- Week 3/task2.java
+++ Week 3/task2.java
@@ -1,17 +1,30 @@
 package newassgn;
 
-public class task2{
+public class task2 {
 
-public static void main(String[] args) {
 	
-		printx("print", "println", "printerr");
+	public static void main(String[] args) {
+	printx();
+	printx2();
+	
+	task2 y=new task2();
+	y.printx3();
 		
 	}
 	
-	public static void printx(String a, String b, String c) {
-		
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(c);
+	static void printx() {
+		System.out.print("line 1");
 	}
+	
+	static void printx2(){
+		System.out.println("line 2");
+	}
+	
+	
+	void printx3(){
+		System.err.println("line 3");
+		
+	}
+	
+	
 }
