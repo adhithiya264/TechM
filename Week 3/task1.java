287395adbdea1e94aee6cccd6f7c3082d2f8db66
Nagaadhithiya <141645478+adhithiya264@users.noreply.github.com>
2025-02-10 14:10:45 +0530

    Create task1.java

---
+++ Week 3/task1.java
@@ -0,0 +1,17 @@
+package newassgn;
+
+public class task1 {
+	
+	    public static void main(String[] args)
+	    {
+	        System.out.print("1 s.out ");
+	        System.out.print("2 s.out");
+	        
+	        System.out.println("1 s.ln");
+	        System.out.println("2 s.ln");
+	        System.out.println("3 s.ln ");
+	        
+	        System.err.print("1 err");
+	        System.err.print("2 err");
+}
+}
