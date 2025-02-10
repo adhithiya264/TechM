d97f91ffd9f6f6e2a9330db76024dc49cf4ae58b
Nagaadhithiya <141645478+adhithiya264@users.noreply.github.com>
2025-02-10 14:18:43 +0530

    Create task2.java

---
+++ Week 3/task2.java
@@ -0,0 +1,17 @@
+package newassgn;
+
+public class task2{
+
+public static void main(String[] args) {
+	
+		printx("print", "println", "printerr");
+		
+	}
+	
+	public static void printx(String a, String b, String c) {
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
