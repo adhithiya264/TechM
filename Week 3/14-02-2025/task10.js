fdc354562504e252684b94582bc187cff9f3cb55
Nagaadhithiya <141645478+adhithiya264@users.noreply.github.com>
2025-02-14 14:33:54 +0530

    Create task10.js

---
+++ Week 3/14-02-2025/task10.js
@@ -0,0 +1,13 @@
+let mathScore = 92;
+
+if (mathScore === 100) {
+  console.log("Phenomenal!");
+} else if (mathScore > 85) {
+  console.log("Excellent!");
+} else if (mathScore >= 71 && mathScore <= 85) {
+  console.log("Good work!");
+} else if (mathScore >= 51 && mathScore <= 70) {
+  console.log("Practice well!");
+} else {
+  console.log("Hard work is needed");
+}
