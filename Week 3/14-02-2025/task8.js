719445227f94c9ad410addad2cfcf0586872814a
Nagaadhithiya <141645478+adhithiya264@users.noreply.github.com>
2025-02-14 14:40:54 +0530

    Update task8.js

--- Week 3/14-02-2025/task8.js
+++ Week 3/14-02-2025/task8.js
@@ -1,5 +1,7 @@
-let prices = [45, 52, 48, 55, 50, 60, 47]; 
+//Get the price of 1 kg tomato from Monday to Sunday, find its average. 
+//If the average is greater than Rs.50, then display, tomato has given good profit. If not, display "No profit from tomato".
 
+let prices = [45, 52, 48, 55, 50, 60, 47]; 
 
 let total = 0;
 for (let i = 0; i < prices.length; i++) {
