cf215c985542d9ed20fc47a765df5646c9ec57f8
Nagaadhithiya <141645478+adhithiya264@users.noreply.github.com>
2025-02-14 14:30:25 +0530

    Create task3.js

---
+++ Week 3/14-02-2025/task3.js
@@ -0,0 +1,15 @@
+const number = 29;
+
+function isPrime(num) {
+    if (num <= 1) return false; 
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false; 
+    }
+    return true;
+}
+
+if (isPrime(number)) {
+    console.log("The number", number, "is a prime number.");
+} else {
+    console.log("The number", number, "is not a prime number.");
+}
