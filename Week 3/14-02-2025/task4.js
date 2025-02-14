11619e092a361509af331eadc46317a486943532
Nagaadhithiya <141645478+adhithiya264@users.noreply.github.com>
2025-02-14 14:38:01 +0530

    Update task4.js

--- Week 3/14-02-2025/task4.js
+++ Week 3/14-02-2025/task4.js
@@ -1,3 +1,5 @@
+//Create an alert to display the address of the user, and get the input from the user for the same
+
 let userAddress = prompt("Please enter your address:");
 
 alert("Your address is: " + userAddress);
