f67746a9471f62a348bb63e1f24a48d05dfd3227
Nagaadhithiya <141645478+adhithiya264@users.noreply.github.com>
2025-02-14 14:41:21 +0530

    Update task9.js

--- Week 3/14-02-2025/task9.js
+++ Week 3/14-02-2025/task9.js
@@ -1,3 +1,6 @@
+//Get the values of year and month. Check if the year is leap year.
+//If yes, check if the month is February. If yes, display "The year is leap year and the month is February with 29 days.". If no, display "The year is leap year and the month is not February ". If the year is not leap, display "The given year is not a leap year".
+
 function isLeapYear(year) {
   return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
 }
