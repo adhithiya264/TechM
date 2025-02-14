6210e726a5c93f2eac15c0921f4de03e292ec1d8
Nagaadhithiya <141645478+adhithiya264@users.noreply.github.com>
2025-02-14 12:40:02 +0530

    Update task.js

--- Week 3/14-02-2025/task.js
+++ Week 3/14-02-2025/task.js
@@ -60,4 +60,57 @@ let userAge = prompt("Please enter your age:");
 alert("Your age is: " + userAge);
 
 
+//task7
+
+let number = 24; 
+if (number % 12 === 0) {
+  console.log(number + " is divisible by 12.");
+} else {
+  console.log(number + " is not divisible by 12.");
+}
+
+
+//task8
+
+
+let prices = [45, 52, 48, 55, 50, 60, 47]; 
+
+
+let total = 0;
+for (let i = 0; i < prices.length; i++) {
+  total += prices[i];
+}
+
+
+let averagePrice = total / prices.length;
+
+
+if (averagePrice > 50) {
+  console.log("Tomato has given good profit.");
+} else {
+  console.log("No profit from tomato.");
+}
+
+
+//task9
+
+
+function isLeapYear(year) {
+  return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+}
+
+let year = 2024;
+let month = 2; 
+
+if (isLeapYear(year)) {
+ 
+  if (month === 2) {
+    console.log("The year is leap year and the month is February with 29 days.");
+  } else {
+    console.log("The year is leap year and the month is not February.");
+  }
+} else {
+  console.log("The given year is not a leap year.");
+}
+
 
