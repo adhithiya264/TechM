44b15283fd64d87db6e1187a40da8a8408a6346f
Nagaadhithiya <141645478+adhithiya264@users.noreply.github.com>
2025-02-14 14:30:03 +0530

    Create task2.js

---
+++ Week 3/14-02-2025/task2.js
@@ -0,0 +1,9 @@
+var number = -7;
+
+if (number > 0) {
+    console.log("The number", number, "is positive.");
+} else if (number < 0) {
+    console.log("The number", number, "is negative.");
+} else {
+    console.log("The number is zero.");
+}
