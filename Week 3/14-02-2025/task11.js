a7b2de2060f10fe6bd404fb9cfe770e10945c6d4
Nagaadhithiya <141645478+adhithiya264@users.noreply.github.com>
2025-02-14 14:34:15 +0530

    Create task11.js

---
+++ Week 3/14-02-2025/task11.js
@@ -0,0 +1,22 @@
+let dayOfWeek = "Monday"; 
+switch(dayOfWeek) {
+  case "Monday":
+    console.log("Start of the week!");
+    break;
+  case "Tuesday":
+  case "Wednesday":
+  case "Thursday":
+    console.log("Mid of the week!");
+    break;
+  case "Friday":
+    console.log("Last working day of the week!");
+    break;
+  case "Saturday":
+    console.log("WeekEnd - Saturday!");
+    break;
+  case "Sunday":
+    console.log("WeekEnd - Sunday!");
+    break;
+  default:
+    console.log("Invalid input!");
+}
