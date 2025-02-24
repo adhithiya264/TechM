6439faaee1288747f5f43c33668f661b9d9730bc
Nagaadhithiya <141645478+adhithiya264@users.noreply.github.com>
2025-02-24 21:07:40 +0530

    Create task1.java

---
+++ Core Java Consolidated Tasks/Taskseven/task1.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class ColorList {
+    public static void main(String[] args) {
+        ArrayList<String> colors = new ArrayList<>();
+        
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Yellow");
+        colors.add("Purple");
+
+        System.out.println("Colors in the list: ");
+        for (String color : colors) {
+            System.out.println(color);
+        }
+    }
+}
