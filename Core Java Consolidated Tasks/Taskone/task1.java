a447832277fe0a16c1d94a28d3bb30c5a44be796
Nagaadhithiya <141645478+adhithiya264@users.noreply.github.com>
2025-02-24 08:18:38 +0530

    Create task1.java

---
+++ Core Java Consolidated Tasks/Taskone/task1.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class CopyAlternateElements {
+    public static void main(String[] args) {
+        
+        int[] originalArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+          
+        int newSize = (originalArray.length + 1) / 2;
+
+        int[] alternateArray = new int[newSize];
+        
+        for (int i = 0, j = 0; i < originalArray.length; i += 2) {
+            alternateArray[j++] = originalArray[i];
+        }
+        
+        System.out.println("Original Array: " + Arrays.toString(originalArray));
+        System.out.println("Array with alternate elements: " + Arrays.toString(alternateArray));
+    }
+}
