12f2b7af150bdebedff0ec5d2857e210b0f8714a
Nagaadhithiya <141645478+adhithiya264@users.noreply.github.com>
2025-02-24 21:24:53 +0530

    Create task10.java

---
+++ Core Java Consolidated Tasks/Taskseven/task10.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ShuffleArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> colors = new ArrayList<>();
+        
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Yellow");
+        colors.add("Purple");
+
+        System.out.println("Original list: " + colors);
+
+        Collections.shuffle(colors); 
+
+        System.out.println("Shuffled list: " + colors);
+    }
+}
