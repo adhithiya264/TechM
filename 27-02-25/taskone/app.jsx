7044969e659cb914ec42a1b803cbfe16f2105e9c
Nagaadhithiya <141645478+adhithiya264@users.noreply.github.com>
2025-02-27 13:33:13 +0530

    Create app.jsx

---
+++ 27-02-25/taskone/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Component1 from './Component1';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Nested Components Example</h1>
+      <Component1 />
+    </div>
+  );
+};
+
+export default App;
