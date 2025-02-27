e905801ee59a6e474c00147eee9bd9e63aeedeae
Nagaadhithiya <141645478+adhithiya264@users.noreply.github.com>
2025-02-27 13:34:52 +0530

    Create Component4.jsx

---
+++ 27-02-25/taskone/Component4.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Component5 from './Component5';
+
+const Component4 = () => {
+  return (
+    <div>
+      <h2>Component 4</h2>
+      <Component5 />
+    </div>
+  );
+};
+
+export default Component4;
