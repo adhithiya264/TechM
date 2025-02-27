48a5c18edb318609d557c54bd3ababbbb52108dd
Nagaadhithiya <141645478+adhithiya264@users.noreply.github.com>
2025-02-27 13:33:42 +0530

    Create Component1.jsx

---
+++ 27-02-25/taskone/Component1.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Component2 from './Component2';
+
+const Component1 = () => {
+  return (
+    <div>
+      <h2>Component 1</h2>
+      <Component2 />
+    </div>
+  );
+};
+
+export default Component1;
